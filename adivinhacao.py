9d3c92a71aa16b53e992a2e4a697b42752e7e4c4
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-07-16 10:33:33 -0300

    aula 3

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,23 @@
+print("********************************")
 print("Bem vindo ao jogo de adivinhação")
+print("********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite seu numero: ")
+print("Você digitou ", chute_str)
+chute = int(chute_str)
+
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
+    print("Você acertou")
+else:
+    if (maior):
+        print("Você errou. O seu chute foi maior que o número secreto.")
+    elif (menor):
+        print("Você errou. O seu chute foi menor que o número secreto.")
+
+print("Fim do jogo")
