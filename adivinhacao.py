1fda19e4f90392056c06079097c0d7ed89424389
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-07-16 18:52:07 -0300

    aula 7

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,10 +5,20 @@ print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
-rodada = 1
+total_de_tentativas = 0
+pontos = 1000
 
-# while (rodada <= total_de_tentativas):
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite seu numero entre 1 e 100: ")
@@ -24,12 +34,16 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou")
+        print("Você acertou e fez {} pontos".format(pontos))
         break
     else:
         if (maior):
             print("Você errou. O seu chute foi maior que o número secreto.")
         elif (menor):
             print("Você errou. O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+        if (rodada == total_de_tentativas):
+            print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
 
 print("Fim do jogo")
