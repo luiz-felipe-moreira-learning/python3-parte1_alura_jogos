351c533afeb77afe3bb736a2a0bf4a1d907e7d77
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-07-17 19:03:01 -0300

    parte 2 aula 1

---
+++ forca.py
@@ -0,0 +1,20 @@
+import random
+
+def jogar():
+    print("**************************")
+    print("Bem vindo ao jogo de Forca")
+    print("**************************")
+
+    print("Fim do jogo")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("jogando...")
+
+if (__name__ == "__main__"):
+    print("Jogando forca")
+    jogar()
