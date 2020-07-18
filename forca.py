0cc79f0c32f6c0b23d2551495b99a52241ea7ec4
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-07-18 18:57:36 -0300

    parte 2 aula 6

--- forca.py
+++ forca.py
@@ -5,24 +5,43 @@ def jogar():
     print("Bem vindo ao jogo de Forca")
     print("**************************")
 
-    print("Fim do jogo")
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip("\n")
+        palavras.append(linha)
+    arquivo.close()
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
     while (not enforcou and not acertou):
         index = 0
         chute = input("Qual a letra? ")
-        chute = chute.strip()
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        chute = chute.strip().upper()
+        if (chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros += 1
+        acertou = "_" not in letras_acertadas
+        enforcou = erros == 6
         print(letras_acertadas)
+
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("você perdeu!")
+    print("Fim do jogo")
+
 
 if (__name__ == "__main__"):
     print("Jogando forca")
